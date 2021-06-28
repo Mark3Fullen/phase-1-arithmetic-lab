8d04d460caa9cd4ece275b9d19331cf2f97192d1
Mark <mark3fullen@gmail.com>
2021-06-28 14:37:10 -0400

    commit tree 7feb459fd8154c0408cac2402dcd8e4f0303985f

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+
+const random = Math.floor(Math.random()) + 1;
+
+let num3 = 9;
+let num4 = 5;
+const mod = 9 % 5;
+
+
+const max = Math.max(1, 16, 17, 18, 19, 20);
+
